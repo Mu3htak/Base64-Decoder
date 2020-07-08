5c2bc351754c34a46f0ebe4a0fd15aadea234fc7
Mushtak Hussain <34830693+Mu3htak@users.noreply.github.com>
2020-07-08 16:50:25 +0530

    Create decoder.py

---
+++ decoder.py
@@ -0,0 +1,17 @@
+import base64,sys
+import argparse, binascii
+from colorama import Fore
+
+parser = argparse.ArgumentParser(description="Decode base64 format")
+parser.add_argument('-d','--decode',nargs='*',required=True,help="Paste base64 string to decode")
+args = parser.parse_args()
+
+try:
+    for i in args.decode:
+        decodedbytes = base64.b64decode(i)
+        decodedstr = str(decodedbytes)
+        print("[%s] Decoded -> %s" %(Fore.RED + i + Fore.RESET,Fore.GREEN + decodedstr))
+        sys.stdout.write(Fore.GREEN + Fore.RESET)
+        sys.stdout.flush()
+except binascii.Error:
+    print("Bad Format -> [%s]" % (Fore.RED + i + Fore.RESET))
