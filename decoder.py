f74bbc1f15a64d739380c4c337c0a376896ecc0b
Mushtak Hussain <34830693+Mu3htak@users.noreply.github.com>
2021-01-01 23:24:35 +0530

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -7,11 +7,14 @@ parser.add_argument('-d','--decode',nargs='*',required=True,help="Paste base64 s
 args = parser.parse_args()
 
 try:
+    
     for i in args.decode:
         decodedbytes = base64.b64decode(i)
         decodedstr = str(decodedbytes, "utf-8")
         print("[%s] Decoded -> %s" %(Fore.RED + i + Fore.RESET,Fore.GREEN + decodedstr))
         sys.stdout.write(Fore.GREEN + Fore.RESET)
         sys.stdout.flush()
+        
 except binascii.Error:
+    
     print("Bad Format -> [%s]" % (Fore.RED + i + Fore.RESET))
